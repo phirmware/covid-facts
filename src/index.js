1042ac72f6dabed1e92d11c8b7a79176e26dd003
phirmware <chibuzor.ojukwu@gmail.com>
2020-03-27 14:46:18 +0100

    added some function to return a number of facts

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ const facts = require('./covid-facts.json');
 const uniqueRandomArray = require('unique-random-array');
 
 function returnNumber(number) {
-    var shuffled = facts.sort(function(){return 0.5 - Math.random()});
-    var selected = shuffled.slice(0,number);
+    const shuffled = facts.sort(function(){return 0.5 - Math.random()});
+    const selected = shuffled.slice(0,number);
     return selected;
 }
 
